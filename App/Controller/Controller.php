a5e53083db0135d5a06a0532e038d8d5e0c2fbf0
Wisnu Aditya <13519093@std.stei.itb.ac.id>
2021-10-10 06:43:27 +0000

    Start Dorayaki.php Controller.php

---
+++ App/Controller/Controller.php
@@ -0,0 +1,18 @@
+
+<?php
+namespace App/Controller
+use PDO;
+class Controller{
+    protected $db;
+
+    public function __construct(){
+        try{
+            $this-> db = new PDO("sqlite":.ROOT. "database/sample.db");
+            $this-> db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this-> db->EXEC("PRAGMA foreign_keys = ON");
+        }
+        catch(\PDOException $e){
+            die("Error!" . $e->getMessage());
+        }
+    }
+}
---
+++ App/Controller/Dorayaki.php
@@ -0,0 +1,8 @@
+<?php
+namespace App/Controller;
+
+class Dorayaki extends Controller{
+    public function __construct(){
+        parent::__construct();
+    }
+}
