737dae91d9fd95ec8deaba86919c4beb76d639a3
Wisnu Aditya <13519093@std.stei.itb.ac.id>
2021-10-21 15:24:26 +0000

    TODO : AJAX

--- App/Handler/BuyDorayaki.php
+++ App/Handler/BuyDorayaki.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'Connect.php';
-    $dorayaki = new App\Controller\Dorayaki();
+    $dorayaki = new App/Controller/Dorayaki();
     if(!isset($_COOKIE['username'])){
         header('Location: Authenticate.php')
     }
@@ -22,11 +22,9 @@
     $price = $data["price"];
     $stock = $data["stock"];
     $imgpath = $data["image_path"];
-    $buy_amount = 0;
     if ($_SERVER["REQUEST_METHOD"] == "POST") {      
         $message_err = "";
-        $buy_amount = trim($_POST["amount"]);
-        $buy_amount * $price      
+        $buy_amount = trim($_POST["amount"]);      
         $intosql =
         "INSERT INTO HISTORY(username, id_dorayaki, dorayaki_name, time, counts)
         VALUES($user, $id_dora, $name , NOW(), $buy_amount)";
@@ -40,4 +38,44 @@
           close();
         }
 ?>
-{TODO : Page}
+<!DOCTYPE HTML>
+
+<head>
+    <title>Buy Dorayaki</title>
+    <link rel="stylesheet" type="text/css" href="../style/BuyDorayaki.css">
+    <script>
+        var id = <?php echo $id_dora;?>;
+    </script>
+    <script src="../App/Handler/BuyDorayaki.js"></script>
+    <script src="../App/Handler/UpdateDorayaki.js"></script>
+</head>
+<body>
+    <div class = "BuyImage">
+        <img class = "dorayaki_img" src="../<?php echo $imgpath?>"> </img>
+    </div>
+    <div class = "DorayakiDetails">
+        <h3><?php echo $name?></h3>
+        <h4>Price : <?php echo $price?></h4>
+        <h4>Description</h4>
+        <p><?php echo $desc?></p>
+    </div>
+    <form id = "BuyForm" method = "post">
+        <h5>Amount to Buy : </h5>
+        <div class = "BuyPage">
+            <div class = "Amount">
+                <button class = "AmountButtons" id="-Button" onclick = "decrement(e, <?php echo $price?>)"></button>
+            </div>
+            <div class ="AmountNum">
+                <input type="number" name = "amount" id ="amount-buy-num" value = 1>
+            </div>
+            <div class= "Amount" style = "float: right;">
+                <button class = "AmountButtons" id="+button" onclick = "increment(e, <?php echo $price ?>, <?php echo $amount ?>);">+</button>
+            </div>
+        </div>
+        <div class = "TotalPrice">
+            <h5>Total Price:</h5>
+            <label id = "TotalPriceLabel">Rp </label>
+            <label id = "TotalPriceLabelNum"><?php echo number_format($price, 2, ',', '.');?></label>
+        </div>
+</body>
+</html>
