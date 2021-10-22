84b606d554c099be96dca54c87e9a322e9c84739
Akeyla Naufal <13519178@std.stei.ib.ac.id>
2021-10-22 06:46:11 +0000

    Update Handler/tambahVarian.html, Handler/tambahVarian.php, Handler/tambahVarianStyle.css files

---
+++ Handler/tambahVarian.php
@@ -0,0 +1,47 @@
+<?php
+    /*if(!isset($_COOKIE["username"])){
+        header("Location: login.html");
+    }
+    $user = $_COOKIE["username"];*/
+    try {
+            $db = new PDO('sqlite:../../Database/dorayakuy.db');
+    } catch(PDOException $e){
+        die("Error!" . $e->getMessage());   
+    }
+
+    $nama_varian = $_POST['nama-varian'];
+    $harga_varian = $_POST['harga-varian'];
+    $stok_awal_varian = $_POST['stok-awal-varian'];
+    $deskripsi_varian = $_POST['deskripsi-varian'];
+    $file_gambar = $_FILES['gambar-varian'];
+
+    // Pindahkan gambar ke folder database
+    $upload_dir = '../../Database/images/';
+    if($file_gambar['error'] == UPLOAD_ERR_OK){
+        $tmp_name = $file_gambar['tmp_name'];
+        $file = $file_gambar['name'];
+        $path = pathinfo($file);
+        $file_name = $path['filename'];
+        $extension = $path['extension'];
+        $target_path = $upload_dir . $file_name . '.' . $extension;
+        move_uploaded_file($tmp_name, $target_path);
+        
+        $sql_stmt = $db->prepare("INSERT INTO DORAYAKI(name, desc, price, stock, image_path)
+        VALUES(?, ?, ?, ?, ?)");
+        if($sql_stmt->execute(array($nama_varian, $deskripsi_varian, $harga_varian, $stok_awal_varian, $target_path))){
+            echo "<script type='text/javascript'>alert('Varian berhasil ditambahkan');</script>";
+            header('Location: TambahVarian.html');
+        } else {
+            echo "<script type='text/javascript'>alert('Varian gagal ditambahkan');</script>";
+            header('Location: TambahVarian.html');
+        }
+    } else {
+        echo "<script type='text/javascript'>alert('Tidak ada gambar yang diunggah');</script>";
+        header('Location: TambahVarian.html');
+    }
+
+
+
+
+
+?>
