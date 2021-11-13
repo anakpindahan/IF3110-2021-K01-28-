67578f1ecc69034e6cf4f8153fead0c7b4a8f6a8
La Ode Rajuh Emoko <13519170@std.stei.itb.ac.id>
2021-11-13 01:39:30 +0000

    masukkan data pembelian ataupun pengeditan stock

--- Handler/UpdateStock.php
+++ Handler/UpdateStock.php
@@ -1,11 +1,12 @@
 <?php
-    //$user = $_COOKIE["username"];
+    $user = $_COOKIE["username"];
+    $is_admin = $_COOKIE["is_admin"];
     try {
         $db = new PDO('sqlite:../Databases/dorayakuy.db');
     } catch(PDOException $e){
         die("Error!" . $e->getMessage());   
     }
-    // Anggap ambil id varian dorayaki dari GET
+    // Anggap ambil id varian dorayaki dari POST
     $id_dora = $_POST['id'];
     $val = $_POST['stokDibeli'];
     $val = intval($val);
@@ -18,7 +19,31 @@
     $price = $data["price"];
     $stock = $data["stock"];
     $imgpath = $data["image_path"];
-    $stmt = $db->prepare("UPDATE DORAYAKI SET name=?,desc=?,price=?,stock=?,image_path=? WHERE id=?");
-    $stmt->execute(array($name,$desc,$price,$stock - $val,$imgpath, $id_dora));
-    echo "Success";
+    $date = date('Y-m-d H:i:s');
+
+    if($is_admin == 0){
+        $stmt = $db->prepare("UPDATE DORAYAKI SET name=?,desc=?,price=?,stock=?,image_path=? WHERE id=?");
+        $stmt->execute(array($name,$desc,$price,$stock - $val,$imgpath, $id_dora));
+        $query = "INSERT into HISTORY (username, id_dorayaki, dorayaki_name, time, counts) values ('$user', '$id_dora', '$name', '$date', '$val')";
+        $statement = $db->prepare($query);
+        if($statement->execute()){
+            echo "Success";
+        }
+        else{
+            echo "terjadi error";
+        }
+    }
+    else{
+        $stmt = $db->prepare("UPDATE DORAYAKI SET name=?,desc=?,price=?,stock=?,image_path=? WHERE id=?");
+        $stmt->execute(array($name,$desc,$price,$val,$imgpath, $id_dora));
+        $query = "INSERT into HISTORY (username, id_dorayaki, dorayaki_name, time, counts) values ('$user', '$id_dora', '$name', '$date', '$val-$stock')";
+        $statement = $db->prepare($query);
+        if($statement->execute()){
+            echo "Success";
+        }
+        else{
+            echo "terjadi error";
+        }  
+    }
+    
 ?>
