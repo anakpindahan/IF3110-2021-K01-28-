904aae0f8313c48df37a9ad1821903f0e866c70c
La Ode Rajuh Emoko <13519170@std.stei.itb.ac.id>
2021-11-13 06:22:13 +0000

    Menambahkan navbar dan styling untuk halaman pembelian dan edit stock

--- Handler/BuyDorayaki.php
+++ Handler/BuyDorayaki.php
@@ -27,67 +27,110 @@
 
 <head>
     <title>Buy Dorayaki</title>
-    <!-- <link rel="stylesheet" type="text/css" href="../style/BuyDorayaki.css"> -->
+    <link rel="stylesheet" href="BuyDorayaki.css">
     <script>
         var id = <?php echo $id_dora;?>;
     </script>
     <script src="BuyDorayaki.js"></script>
 </head>
 <body>
-    <div class = "BuyImage">
-        <img class = "dorayaki_img" src="<?php echo $imgpath?>"> </img>
-    </div> 
-    <div class = "DorayakiDetails">
-        <h3><?php echo $name?></h3>
-        <Label> <h4>Harga: </h4></Label>
-        <h4 id="hargaSatuan"><?php echo $price?></h4>
-        <h4>Description</h4>
-        <p><?php echo $desc?></p>
-        <p>Stock: </p>
-        <p id="BatasStok"> <?php echo $stock?></p>
+<div class = "navbar">
+        <div class="logo">
+                <a class="brand" href="dashboard.php">Dorayakuy!</a>
+        </div>
+        <div class="search-bar">
+            <form method="GET" action="HalamanPencarian.php">
+                <input class="kotak-pencarian" type="text" name = "keyword" placeholder="cari dorayaki berdasarkan nama">
+                <input type ="submit" value = "search">
+            </form>
+        </div>
+        <div class = "user-type">
+            <div class="tombol">
+                <?php
+                    if($_COOKIE["is_admin"] == 1){
+                        echo '<a href="TambahVarianForm.php">Tambah varian</a>';
+                        echo '<div>|</div>';
+                        echo'<a href="RiwayatPembelian.php"> Riwayat pembelian</a>';
+                    }
+                    else{
+                        echo'<a href="RiwayatPembelian.php"> Riwayat pembelian</a>';
+                    }
+                ?>
+
+            </div>
+            <div>
+                |
+            </div>
+
+            <div class="tombol">
+                <a href="login.html"> logout </a>
+            </div> 
+        </div>    
     </div>
-    <div id = "BuyForm">
-        <?php
-            if ($_COOKIE['is_admin'] == 0){
-                echo "<h5>Amount to Buy : </h5>";
-            }
-            else{
-                echo "<h5>Masukkan Banyaknya Dorayaki Ini Yang Tersedia : </h5>";
-            }
-        ?>
-        <div class = "BuyPage">
-            <div class = "Amount">
-                <button class = "AmountButtons" id="-Button" onclick = "minus()">-</button>
+
+    <div class="variant-description">
+        <img class = "dorayaki_img" src="<?php echo $imgpath?>">
+        <div class = "DorayakiDetails">
+            <h3>Nama Varian: <?php echo $name?></h3>
+            <div class="Sebaris">
+                <p class="label"> Harga Satuan:</p>
+                <p id="hargaSatuan"><?php echo $price?></p>
             </div>
-            <div class ="AmountNum">
-                <input type="number" name = "amount" id ="amount-buy-num" value = 1 oninput = <?php if($_COOKIE['is_admin'] == 0){echo "updateTotalHarga()";}?>>
+            <div class="Sebaris">
+                <p class="label">Deskripsi:</p>
+                <p><?php echo $desc?></p>
             </div>
-            <div class= "Amount">
-                <button class = "AmountButtons" id="+button" onclick = "<?php if($_COOKIE['is_admin'] == 0){echo "plus()";} else{echo "plusAdmin()";}?>">+</button>
+            <div class = "Sebaris">
+                <p class="label">Stock: </p>
+                <p id="BatasStok"> <?php echo $stock?></p>
+            </div>
+            <div id = "BuyForm">
+                <?php
+                    if ($_COOKIE['is_admin'] == 0){
+                        echo "<h5>Amount to Buy : </h5>";
+                    }
+                    else{
+                        echo "<h5>Masukkan Banyaknya Dorayaki Ini Yang Tersedia : </h5>";
+                    }
+                ?>
+                <div class = "BuyPage">
+                    <div class = "Amount">
+                        <button class = "AmountButtons" id="-Button" onclick = "minus()">-</button>
+                    </div>
+                    <div class ="AmountNum">
+                        <input type="number" name = "amount" id ="amount-buy-num" value = 1 oninput = <?php if($_COOKIE['is_admin'] == 0){echo "updateTotalHarga()";}?>>
+                    </div>
+                    <div class= "Amount">
+                        <button class = "AmountButtons" id="+button" onclick = "<?php if($_COOKIE['is_admin'] == 0){echo "plus()";} else{echo "plusAdmin()";}?>">+</button>
+                    </div>
+                </div>
+                <div class = "TotalPrice">
+                    <?php
+                        if ($_COOKIE['is_admin'] == 0){
+                            echo '<h5 id="totalPriceText">Total Price:</h5>
+                            <h5 id = "TotalPriceLabelNum">'
+                            .$price.
+                            '</h5>';
+                        }
+                    ?>
+                </div>
+                <button id="BuyButton" class = "BuyButton" onclick = "buyy()">
+                    <?php 
+                        if ($_COOKIE['is_admin'] == 0){
+                            echo "Buy";
+                        }
+                        else{
+                            echo "Edit Stock";
+                        }
+                    ?>
+                </button>
+                <label id="pesanPembelian" for="BuyButton"></label>
             </div>
         </div>
+        
     </div>
-        <div class = "TotalPrice">
-            <?php
-                if ($_COOKIE['is_admin'] == 0){
-                    echo '<h5 id="totalPriceText">Total Price:</h5>
-                    <label for="totalPriceText" id = "TotalPriceLabelNum">'
-                    .$price.
-                    '</label>';
-                }
-            ?>
-        </div>
-        <button id="BuyButton" class = "BuyButton" onclick = "buyy()">
-            <?php 
-                if ($_COOKIE['is_admin'] == 0){
-                    echo "Buy";
-                }
-                else{
-                    echo "Edit Stock";
-                }
-            ?>
-        </button>
-        <label id="pesanPembelian" for="BuyButton"></label>
+        
+        
         <script>
             function buyy(){
                 var xhttp = new XMLHttpRequest();
